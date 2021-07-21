5f6fb761a7f68cafe31b29c3e0747612aa59edcc
unknown <sanjeevsingh.77d@gmail.com>
2021-07-22 01:01:41 +0530

    tossed which player play

--- tictaktoi.js
+++ tictaktoi.js
@@ -1,4 +1,6 @@
-console.log('Welcome to tictaktoi game');
+console.log('Welcome to tictaktoi game'); 
+let player = 0;
+let system = 1;
 function board() {
 
     console.log("     |   |");
@@ -9,3 +11,15 @@ function board() {
 
 }
 board();
+function toss() {
+    let toss = Math.round(Math.random())
+    console.log(toss)
+
+    if (toss == 0) {
+        console.log('player won the toss and his chance first');
+    }
+    else {
+        console.log('system chance');
+    }
+}
+toss()
