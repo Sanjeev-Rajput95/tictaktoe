5e03bec4e4bcf129237b8a16ba6bfa296277a6d7
unknown <sanjeevsingh.77d@gmail.com>
2021-07-22 00:19:44 +0530

    Printing tictaktoi board

---
+++ tictaktoi.js
@@ -0,0 +1,11 @@
+console.log('Welcome to tictaktoi game');
+function board() {
+
+    console.log("     |   |");
+    console.log(" ...............");
+    console.log("     |   |");
+    console.log(" ...............");
+    console.log("     |   |");
+
+}
+board();
